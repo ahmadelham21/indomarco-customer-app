0e92e3bcb9d53a7fe108c830c34bd0d0670b60ed
ahmadelham21 <ahmadelham21@gmail.com>
2025-07-29 14:21:35 +0700

    "[UPDATE]updating Customer entity"

--- CustomerApp.Domain/Entities/Customer.cs
+++ CustomerApp.Domain/Entities/Customer.cs
@@ -9,13 +9,13 @@ namespace CustomerApp.Domain.Entities
 {
     public class Customer
     {
-        public int CustomerId { get; set; } // Sesuai dengan customerld SERIAL PRIMARY KEY [cite: 11]
-        public string CustomerCode { get; set; } // [cite: 12]
-        public string CustomerName { get; set; } // [cite: 13]
-        public string CustomerAddress { get; set; } // [cite: 14]
-        public int CreatedBy { get; set; } // [cite: 15]
-        public DateTime CreatedAt { get; set; } // [cite: 16]
-        public int? ModifiedBy { get; set; } // Nullable [cite: 17]
-        public DateTime? ModifiedAt { get; set; } // Nullable [cite: 18]
+        public int CustomerId { get; set; } 
+        public string CustomerCode { get; set; } 
+        public string CustomerName { get; set; } 
+        public string CustomerAddress { get; set; } 
+        public int CreatedBy { get; set; } 
+        public DateTime CreatedAt { get; set; } 
+        public int? ModifiedBy { get; set; } 
+        public DateTime? ModifiedAt { get; set; } 
     }
 }
